b65210d7b51eb7fdb6ef0130f076b46071f07ace
KhushPatibandha <khush.patibandha@gmail.com>
2024-06-08 14:59:24 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,17 +7,18 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-
-	var s, err = bufio.NewReader(os.Stdin).ReadString('\n');
-	s = s[:len(s)-1]
-	if s[len(s)-1] == '\r' {
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		var s, err = bufio.NewReader(os.Stdin).ReadString('\n');
 		s = s[:len(s)-1]
+		if s[len(s)-1] == '\r' {
+			s = s[:len(s)-1]
+		}
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		
+		fmt.Print(s, ": command not found\n")
 	}
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Print(s, ": command not found\n")
 }
